26699515155eb8fa9a3e67651dfe4873f091a767
LinLicoa <villegas.licoa@gmail.com>
2023-11-24 02:43:13 -0500

    arreglos

--- src/app/components/main/post-creation/post-creation.component.ts
+++ src/app/components/main/post-creation/post-creation.component.ts
@@ -6,5 +6,26 @@ import { Component } from '@angular/core';
   styleUrls: ['./post-creation.component.css']
 })
 export class PostCreationComponent {
+ // Método para manejar la selección de archivos
+onFileSelect(event: Event): void {
+  const element = event.currentTarget as HTMLInputElement;
+  let fileList: FileList | null = element.files;
 
+  if (fileList) {
+    const file: File = fileList[0];
+    console.log("Archivo seleccionado:", file);
+    // Procesa el archivo aquí
+  } else {
+    // Maneja la situación de que no se haya seleccionado ningún archivo
+    console.log("No se ha seleccionado ningún archivo.");
+  }
+}
+
+
+  // Método para manejar el envío del formulario
+  submitForm(): void {
+    // Aquí implementarías la lógica para procesar los datos del formulario
+    console.log("Formulario enviado");
+    // Por ejemplo, enviar los datos a un servidor o validar los campos
+  }
 }
--- src/app/components/main/profile/profile.component.ts
+++ src/app/components/main/profile/profile.component.ts
@@ -6,5 +6,61 @@ import { Component } from '@angular/core';
   styleUrls: ['./profile.component.css']
 })
 export class ProfileComponent {
+  fotosCreadas: string[] = ['img1.jpg','img2.jpg','img3.jpg','img4.jpg','img5.jpg','img6.jpg','img7.jpg','img8.jpg','img9.jpg','img10.jpg','img11.jpg','img12.jpg','img13.jpg','img14.jpg','img15.jpg' ];
 
+  pestanaActual: string = 'creado'; // Inicializa la pestaña actual como "creado"
+  mostrarPestana(pestana: string): void {
+    this.pestanaActual = pestana;
+  }
+
+   // Propiedades del perfil
+   nombre: string = 'Nombre';
+   apellido: string = 'Apellido';
+   usuario: string = 'nombre_usuario';
+   mostrarFormulario: boolean = false;
+   
+ 
+   // Propiedades para el formulario de edición
+   nuevoNombre: string = '';
+   nuevoApellido: string = '';
+   nuevoUsuario: string = '';
+   nuevaFoto: File | null = null;
+
+   
+   abrirFormularioEdicion(): void {
+     this.mostrarFormulario = true;
+     this.nuevoNombre = this.nombre;
+     this.nuevoApellido = this.apellido;
+     this.nuevoUsuario = this.usuario;
+   }
+ 
+   cerrarFormularioEdicion(): void {
+     this.mostrarFormulario = false;
+   }
+ 
+   guardarCambios(): void {
+     this.nombre = this.nuevoNombre;
+     this.apellido = this.nuevoApellido;
+     this.usuario = this.nuevoUsuario;
+     this.cerrarFormularioEdicion();
+   }
+
+   cargarNuevaFoto(event: any): void {
+    const files: FileList | null = event.target.files;
+    if (files && files.length > 0) {
+      this.nuevaFoto = files[0];
+    }
+  }
+
+  compartir(): void {
+    const currentURL: string = window.location.href;
+    // Copiar la URL al portapapeles
+    navigator.clipboard.writeText(currentURL)
+    .then(() => {
+      console.log('URL copiada al portapapeles: ' + currentURL);
+    })
+    .catch((error) => {
+      console.error('Error al copiar la URL: ', error);
+    });
+  }
 }
